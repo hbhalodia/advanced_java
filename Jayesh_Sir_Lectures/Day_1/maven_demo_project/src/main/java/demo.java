9f5912eb46cbadd00d2118d89f44c6b96344b9b6
Hit Bhalodia <hit.bhalodia@rtcamp.com>
2021-07-17 17:08:08 +0530

    My first Commit

---
+++ Jayesh_Sir_Lectures/Day_1/maven_demo_project/src/main/java/demo.java
@@ -0,0 +1,15 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class demo {
+    public static void main(String[] args) {
+        try{
+            Connection connection = DriverManager.getConnection("");
+
+
+        }
+        catch (Exception e){
+
+        }
+    }
+}
---
+++ Jayesh_Sir_Lectures/Day_2/Crud_operations/src/main/java/adv_java_funda/utils/dbutils.java
@@ -0,0 +1,36 @@
+package adv_java_funda.utils;
+
+import java.sql.*;
+
+public class dbutils {
+    //get Conenection
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/mu_students";
+    private static final String DB_USER = "root";
+    private static final String DB_PASS = "";
+
+    public static Connection getConnection() throws SQLException {
+        try(Connection connection = DriverManager.getConnection(DB_URL,DB_USER,DB_PASS)){
+            return connection;
+        }catch (Exception e){
+            e.printStackTrace();
+            System.err.println("Error While Connecting DB : "+e);
+        }
+        return null;
+    }
+
+    public static int createEntry(Connection connection , String sql , String name , String email , int roll_no) throws SQLException{
+        PreparedStatement prepStat = connection.prepareStatement(sql);
+
+        prepStat.setInt(1,roll_no);
+        prepStat.setString(2,name);
+        prepStat.setString(3,email);
+
+        return prepStat.executeUpdate();
+    }
+
+    public static ResultSet readEntries(Connection connection, String sql) throws SQLException {
+        PreparedStatement prepStat = connection.prepareStatement(sql);
+
+        return prepStat.executeQuery();
+    }
+}
